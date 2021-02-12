8ec1ed652ff5c87a3783db11770098cbc49e4c88
Vishesh Singh <VisheshSingh16@gmail.com>
2021-02-11 18:41:04 -0600

    async/await 🤩 to fetch joke

--- dad-jokez/script.js
+++ dad-jokez/script.js
@@ -5,7 +5,7 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+/* function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
@@ -15,4 +15,15 @@ function generateJoke() {
   fetch(`https://icanhazdadjoke.com/`, config)
     .then((res) => res.json())
     .then((data) => (jokeEl.innerHTML = data.joke));
+} */
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+  const res = await fetch(`https://icanhazdadjoke.com/`, config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
 }

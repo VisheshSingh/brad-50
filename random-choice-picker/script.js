5682409338749127dcdbdcdb8f1436663be010c8
Vishesh Singh <VisheshSingh16@gmail.com>
2021-02-13 23:36:06 -0600

    Pick a random tag 🥶

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -5,6 +5,11 @@ textarea.focus();
 
 textarea.addEventListener('keydown', (e) => {
   createTag(e.target.value);
+
+  if (e.key === 'Enter') {
+    randomSelect();
+    e.target.value = '';
+  }
 });
 
 function createTag(input) {
@@ -21,4 +26,39 @@ function createTag(input) {
     tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const times = 30;
+
+  let interval = setInterval(() => {
+    let random = pickRandomTag();
+    highlight(random);
+
+    setTimeout(() => {
+      unhighlight(random);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      let random = pickRandomTag();
+      highlight(random);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlight(tag) {
+  tag.classList.add('highlight');
+}
+
+function unhighlight(tag) {
+  tag.classList.remove('highlight');
 }

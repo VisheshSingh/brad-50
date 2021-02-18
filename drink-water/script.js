b370c714744492bbbc13a199a938849dbbb8cf0c
Vishesh Singh <VisheshSingh16@gmail.com>
2021-02-17 22:49:28 -0600

    Update big cup + percentage 🥤

--- drink-water/script.js
+++ drink-water/script.js
@@ -1,7 +1,31 @@
 const smallCups = document.querySelectorAll('.cup-small');
-const liters = document.getElementById('.liters');
-const remained = document.getElementById('.remained');
-const percentage = document.getElementById('.percentage');
+const liters = document.getElementById('liters');
+const remained = document.getElementById('remained');
+const percentage = document.getElementById('percentage');
+
+console.log(percentage);
+
+function updateBugCup() {
+  const total = smallCups.length;
+  const filled = document.querySelectorAll('.full').length;
+  if (filled === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.innerText = `${(filled / total) * 100}%`;
+    percentage.style.height = `${(filled / total) * 100}%`;
+  }
+
+  if (filled === total) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    remained.style.height = 'auto';
+    liters.innerText = `${(2000 - filled * 250) / 1000}L`;
+  }
+}
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => fillCup(idx));
@@ -10,7 +34,7 @@ smallCups.forEach((cup, idx) => {
 function fillCup(idx) {
   if (
     smallCups[idx].classList.contains('full') &&
-    !smallCups[idx].nextElementSibling.classList.contains('full')
+    !smallCups[idx].nextElementSibling?.classList.contains('full')
   ) {
     idx--;
   }
@@ -21,4 +45,6 @@ function fillCup(idx) {
       cup.classList.remove('full');
     }
   });
+
+  updateBugCup();
 }

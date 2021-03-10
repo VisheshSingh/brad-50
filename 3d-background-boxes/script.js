b06372e53178fb60bb68f25ca145729389494fc6
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-10 16:20:45 -0600

    rotating 3d boxes 🟨

---
+++ 3d-background-boxes/script.js
@@ -0,0 +1,13 @@
+const magicBtn = document.getElementById('btn');
+const boxes = document.getElementById('boxes');
+
+for (let i = 0; i < 4; i++) {
+  for (let j = 0; j < 4; j++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.backgroundPosition = `-${j * 125}px -${i * 125}px`;
+    boxes.appendChild(box);
+  }
+}
+
+magicBtn.addEventListener('click', () => boxes.classList.toggle('big'));

1aea3b4014e803443e379be80427e8aa110e33cb
Vishesh Singh <VisheshSingh16@gmail.com>
2021-03-23 21:36:42 -0500

    wip

--- insect-game/script.js
+++ insect-game/script.js
@@ -1,1 +1,7 @@
 const screens = document.querySelectorAll('.screen');
+const playBtn = document.getElementById('btn');
+const chooseInsects = document.querySelectorAll('.choose-insect-btn');
+
+playBtn.addEventListener('click', () => {
+  screens[0].classList.add('up');
+});
